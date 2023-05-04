a13bf4e7a12579e6b18e19f514b99032e0c9a559
Alina N <silumain@gmail.com>
2023-05-04 14:53:02 +0300

    stage 10.12

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,12 +6,14 @@ CONDITION = 'Answer "yes" if given number is prime. ' \
 
 
 def ask_question():
-    num = random.randint(2, 1000)
+    num = random.randint(1, 1000)
     question = str(num)
     for i in range(2, num):
         if num % i == 0:
             correct_answer = 'no'
             return question, correct_answer
         else:
-            correct_answer = 'yes'
-            return question, correct_answer
+            continue
+    else:
+        correct_answer = 'yes'
+        return question, correct_answer
